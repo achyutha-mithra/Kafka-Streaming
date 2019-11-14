c8a166d9ec68307802adc427e730c52de2366eac
achyutha-mithra <56015756+achyutha-mithra@users.noreply.github.com>
2019-11-14 11:44:00 +0530

    Create Consumer.scala

---
+++ KafkaStreamFromFile/src/Consumer.scala
@@ -0,0 +1,41 @@
+import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.apache.kafka.common.serialization.StringDeserializer
+
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming._
+import org.apache.spark.streaming.kafka010._
+import org.apache.log4j._
+
+class Consumer(topics: String, bootstrap_server: String, group_name: String) {
+ 
+    Logger.getLogger("org").setLevel(Level.ERROR)
+
+    val sparkConf = new SparkConf().setAppName("DirectKafkaWordCount")
+    .setMaster("local[*]")
+    .set("spark.executor.memory","1g")
+    
+    val ssc = new StreamingContext(sparkConf, Seconds(1))
+
+    val topicsSet = topics.split(",")
+    
+    val kafkaParams = Map[String, Object](
+      ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG -> bootstrap_server,
+      ConsumerConfig.GROUP_ID_CONFIG -> group_name,
+      ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG -> classOf[StringDeserializer],
+      ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG -> classOf[StringDeserializer],
+      "auto.offset.reset" ->"earliest")
+      
+      
+    val messages = KafkaUtils.createDirectStream[String, String](
+      ssc,
+      LocationStrategies.PreferConsistent,
+      ConsumerStrategies.Subscribe[String, String](topicsSet, kafkaParams))
+
+    val lines = messages.map(_.value)
+    val words = lines.flatMap(_.split(" "))
+    val wordCounts = words.map(x => (x, 1)).reduceByKey(_ + _)
+    wordCounts.print()
+
+    ssc.start()
+    ssc.awaitTermination()
+}
